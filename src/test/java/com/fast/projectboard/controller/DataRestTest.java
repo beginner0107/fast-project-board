6ceb71e581ff41929ac81dad3d3c8aedb7879a4d
Beginner <dkstmdwn17@naver.com>
2022-11-02 22:37:24 +0900

    리팩토링: 사용 안하는 import 정리
    optimize import 하지 않은 듯.

--- src/test/java/com/fast/projectboard/controller/DataRestTest.java
+++ src/test/java/com/fast/projectboard/controller/DataRestTest.java
@@ -5,13 +5,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.client.match.MockRestRequestMatchers;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
